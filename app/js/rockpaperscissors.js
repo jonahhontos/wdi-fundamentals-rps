6d1a580c0e9e90925171c2342b36a855b71bf47b
Jonah Hontos <jonahhontos@gmail.com>
2015-12-23 15:15:24 -0800

    added chapter 4  homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -43,6 +43,44 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    switch (playerMove) {
+        case 'rock':
+            switch (computerMove) {
+                case 'rock':
+                    winner = 'tie';
+                    break;
+                case 'paper':
+                    winner = 'computer';
+                    break;
+                case 'scissors':
+                    winner = 'player';
+            }
+            break;
+        case 'paper':
+            switch (computerMove) {
+                case 'rock':
+                    winner = 'player';
+                    break;
+                case 'paper':
+                    winner = 'tie';
+                    break;
+                case 'scissors':
+                    winner = 'computer';
+            }
+            break;
+        case 'scissors':
+            switch (computerMove) {
+                case 'rock':
+                    winner = 'computer';
+                    break;
+                case 'paper':
+                    winner = 'player';
+                    break;
+                case 'scissors':
+                    winner = 'tie';
+            }
+            break;
+    }
     return winner;
 }
 
